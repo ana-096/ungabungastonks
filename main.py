6e0e717b9c5ebe7405575a265ede76e90c20b171
ana-096 <john.sharples.888@gmail.com>
2023-06-05 13:13:59 +1000

    did some stuff

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import glob
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 
-START_DATE = ""
+TRAINING_START_DATE = "2022"
+TRAINING_END_DATE = "2023"
 
 path = "./*.csv"
 files = glob.glob(path)
@@ -13,5 +15,21 @@ stonks = {
     name: stock for name, stock in zip(names, [pd.read_csv(file) for file in files])
 }
 
-for stock in stonks.values():
-    pass
+training_stonks = {}
+
+for name, stonk in stonks.items():
+    # convert dates to datetime for comparisons
+    stonk['Date'] = pd.to_datetime(stonk['Date'], dayfirst=False)
+
+    # select only date and close
+    training_stonks[name] = stonk[stonk['Date'].isin(pd.date_range(TRAINING_START_DATE, TRAINING_END_DATE))][['Date','Close/Last']]
+
+    # convert close to int in cents
+    training_stonks[name]['Close/Last'] = training_stonks[name]['Close/Last'].str.replace("$", "")
+    training_stonks[name]['Close/Last'] = training_stonks[name]['Close/Last'].str.replace(".", "")
+    training_stonks[name]['Close/Last'] = training_stonks[name]['Close/Last'].map(lambda x: int(x))
+
+for name, training_stonk in training_stonks.items():
+    training_stonk.plot(title = name,x = 'Date', y = 'Close/Last')
+
+plt.show()
