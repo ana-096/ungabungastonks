64a5bb80e01402a8bcfbe410dc560cf2d25c108f
ana-096 <john.sharples.888@gmail.com>
2023-06-05 12:20:38 +1000

    more download

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
+import glob
 import numpy as np
 import pandas as pd
+
+START_DATE = ""
+
+path = "./*.csv"
+files = glob.glob(path)
+
+names = [x[2:].split("_")[0] for x in files]
+
+stonks = {
+    name: stock for name, stock in zip(names, [pd.read_csv(file) for file in files])
+}
+
+for stock in stonks.values():
+    pass
