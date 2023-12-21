f866e0c31dd38f6b7453b767bca2790d45147595
ana-096 <john.sharples.888@gmail.com>
2023-12-22 01:44:20 +1100

    another tidy

---
+++ sanitise.py
@@ -0,0 +1,20 @@
+import glob
+import pandas as pd
+
+path = "./*.csv"
+files = glob.glob(path)
+
+names = [x[2:].split("_")[0] for x in files]
+
+stocks = {
+    name: stock for name, stock in zip(names, [pd.read_csv(file) for file in files])
+}
+
+for name, stock in stocks.items():
+    # remove $ signs
+    for column in stock.columns:
+        if stock[column].dtype == pd.StringDtype:
+            stock[column] = stock[column].str.replace("$", "")
+    # convert dates to datetime for comparisons
+    stock['Date'] = pd.to_datetime(stock['Date'], dayfirst=False)
+    stock.to_csv(name+".csv")
