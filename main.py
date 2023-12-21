5674b7caa244c98157985aa48587276040c2d919
ana-096 <john.sharples.888@gmail.com>
2023-12-22 01:17:45 +1100

    tidy

--- main.py
+++ main.py
@@ -20,16 +20,17 @@ training_stonks = {}
 for name, stonk in stonks.items():
     # convert dates to datetime for comparisons
     stonk['Date'] = pd.to_datetime(stonk['Date'], dayfirst=False)
+    stonk.rename(columns={'Close/Last': 'Close'}, inplace=True)
 
     # select only date and close
-    training_stonks[name] = stonk[stonk['Date'].isin(pd.date_range(TRAINING_START_DATE, TRAINING_END_DATE))][['Date','Close/Last']]
+    training_stonks[name] = stonk[stonk['Date'].isin(pd.date_range(TRAINING_START_DATE, TRAINING_END_DATE))][['Date','Close']]
 
     # convert close to int in cents
-    training_stonks[name]['Close/Last'] = training_stonks[name]['Close/Last'].str.replace("$", "")
-    training_stonks[name]['Close/Last'] = training_stonks[name]['Close/Last'].str.replace(".", "")
-    training_stonks[name]['Close/Last'] = training_stonks[name]['Close/Last'].map(lambda x: int(x))
+    training_stonks[name]['Close'] = training_stonks[name]['Close'].str.replace("$", "")
+    training_stonks[name]['Close'] = training_stonks[name]['Close'].str.replace(".", "")
+    training_stonks[name]['Close'] = training_stonks[name]['Close'].map(lambda x: int(x))
 
 for name, training_stonk in training_stonks.items():
-    training_stonk.plot(title = name,x = 'Date', y = 'Close/Last')
+    training_stonk.plot(title = name,x = 'Date', y = 'Close')
 
 plt.show()
